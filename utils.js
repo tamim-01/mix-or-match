3d771ac79cb40c2aab1e747ed4d55abcf1fdb52c
tamim-01 <161085907+tamim-01@users.noreply.github.com>
2024-02-24 01:20:53 +0330

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,32 @@
+export async function getAPI (url){
+  let controller = new AbortController();
+  let fetchAbort = setTimeout(()=> controller.abort() , 5000);
+ try {
+    const response = await fetch(url , {signal : controller.signal})
+    if(response.ok){
+      clearTimeout(fetchAbort);
+      const result = await response.json();
+      return result;
+    }else {
+       console.log("api faced an error");
+    }
+}catch (err){
+  console.log("api faced an error ");
+}
+};
+
+
+ export function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+      
+     
+
+    };
+
+   
+    return array;
+  };
+
+  
